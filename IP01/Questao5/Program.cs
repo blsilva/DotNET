359e97dedddc2ae5d74c596ab5875059d04c1a15
Biancardy <biancard.lima@gmail.com>
2023-11-17 16:44:22 -0300

    Questao 5

---
+++ IP01/Questao5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 8;
+
+        if (a > b)
+        {
+            Console.WriteLine($"{a} é maior que {b}");
+        }
+        else
+        {
+            Console.WriteLine($"{a} não é maior que {b}");
+        }
+    }
+}
