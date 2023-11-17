5287272104d0ff4e88c83e9fda968bf8e0c08ed0
Biancardy <biancard.lima@gmail.com>
2023-11-17 16:47:36 -0300

    Questao 6

---
+++ IP01/Questao6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str1 = "Hello";
+        string str2 = "World";
+
+        if (str1.Equals(str2))
+        {
+            Console.WriteLine($"{str1} é igual a {str2}");
+        }
+        else
+        {
+            Console.WriteLine($"{str1} não é igual a {str2}");
+        }
+
+        if (str1 == str2)
+        {
+            Console.WriteLine($"{str1} é igual a {str2}");
+        }
+        else
+        {
+            Console.WriteLine($"{str1} não é igual a {str2}");
+        }
+    }
+}
