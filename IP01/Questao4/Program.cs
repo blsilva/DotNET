2cf319a72c1bd523dd7b8d14ccd3199894d05475
Biancardy <biancard.lima@gmail.com>
2023-11-17 16:39:21 -0300

    Questao 4

---
+++ IP01/Questao4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int x = 10;
+        int y = 3;
+
+        int adicao = x + y;
+        Console.WriteLine($"Adição: {adicao}");
+
+        int subtracao = x - y;
+        Console.WriteLine($"Subtração: {subtracao}");
+
+        int multiplicacao = x * y;
+        Console.WriteLine($"Multiplicação: {multiplicacao}");
+
+        int divisao = x / y;
+        Console.WriteLine($"Divisão: {divisao}");
+
+        double divisaoComDecimal = (double)x / y;
+        Console.WriteLine($"Divisão com parte decimal: {divisaoComDecimal}");
+    }
+}
