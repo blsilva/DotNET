f0c6e30c958822915cb0a8d9719c2c1e47690bf8
Bian Lima <biancard.lima@gmail.com>
2023-11-29 08:59:34 -0300

    Semana tres

---
+++ Semana3/ExemplosAula/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Por favor, insira um número: ");
+            string userInput = Console.ReadLine();
+
+            int numero = Convert.ToInt32(userInput);
+
+            Console.WriteLine($"Você inseriu o número: {numero}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Por favor, insira um número válido.");
+        }
+    }
+}
