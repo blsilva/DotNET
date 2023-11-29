2393c3ec32934208ed7f283fccd5c9bb336ddc03
Bian Lima <biancard.lima@gmail.com>
2023-11-29 09:38:15 -0300

    Exercicio2

---
+++ Semana4/Exercicio2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Veiculo
+{
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+    public string Cor { get; set; }
+
+    public int IdadeVeiculo
+    {
+        get
+        {
+            int anoAtual = DateTime.Now.Year;
+            return anoAtual - Ano;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Veiculo meuVeiculo = new Veiculo();
+
+        meuVeiculo.Modelo = "Carro Modelo X";
+        meuVeiculo.Ano = 2022;
+        meuVeiculo.Cor = "Azul";
+
+        Console.WriteLine("Informações do Veículo:");
+        Console.WriteLine($"Modelo: {meuVeiculo.Modelo}");
+        Console.WriteLine($"Ano: {meuVeiculo.Ano}");
+        Console.WriteLine($"Cor: {meuVeiculo.Cor}");
+        Console.WriteLine($"Idade do Veículo: {meuVeiculo.IdadeVeiculo} anos");
+    }
+}
