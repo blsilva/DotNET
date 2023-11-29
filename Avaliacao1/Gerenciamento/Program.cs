4778fc0cced0a25d63111d1dcae297d9b274545e
Bian Lima <biancard.lima@gmail.com>
2023-11-29 10:33:52 -0300

    primeiro commit

---
+++ Avaliacao1/Gerenciamento/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Medico medico = new Medico("Dr. José", new DateTime(1980, 1, 1), "12345678901", "CRM1234");
+        Console.WriteLine(medico);
+    }
+}
+
+class Pessoa
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string CPF { get; set; }
+
+    public Pessoa(string nome, DateTime dataNascimento, string cpf)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = TratarCPF(cpf);
+    }
+
+    private string TratarCPF(string cpf)
+    {
+        return cpf;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {Nome}, Data de Nascimento: {DataNascimento.ToShortDateString()}, CPF: {CPF}";
+    }
+}
+
+class Medico : Pessoa
+{
+    public string CRM { get; set; }
+
+    public Medico(string nome, DateTime dataNascimento, string cpf, string crm)
+        : base(nome, dataNascimento, cpf)
+    {
+        CRM = crm;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, CRM: {CRM}";
+    }
+}
