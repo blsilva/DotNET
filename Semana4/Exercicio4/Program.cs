41b191247cc07d70770c6b96ec7a565aab5b7185
Bian Lima <biancard.lima@gmail.com>
2023-11-29 10:10:59 -0300

    Exercicio4

---
+++ Semana4/Exercicio4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Aluno
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+    public Aluno()
+    {
+        Nome = "Biancardy Lima";
+        Idade = 29;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Aluno meuAluno = new Aluno();
+
+        Console.WriteLine("Informações do Aluno:");
+        Console.WriteLine($"Nome: {meuAluno.Nome}");
+        Console.WriteLine($"Idade: {meuAluno.Idade}");
+    }
+}
